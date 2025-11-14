54c1bfe25e59b86a8a27e7c8106cec64a145cd6f
pchtech <tech@corporates.com>
2025-11-13 18:02:45 -0600

    Initial commit

---
+++ form-helper-1.meta.js
@@ -0,0 +1,6 @@
+// @name         Form Helper
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @updateURL    https://raw.githubusercontent.com/webtools-scripts/browser-helpers/main/form-helper-1.meta.js
+// @downloadURL  https://raw.githubusercontent.com/webtools-scripts/browser-helpers/main/form-helper-1.user.js
+// ==/UserScript==
