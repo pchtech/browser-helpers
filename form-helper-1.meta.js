4525eb416cc92585fd080f097cc07d23266692cf
pchtech <tech@corporates.com>
2025-11-13 18:06:11 -0600

    Initial release v1.0.1

--- form-helper-1.meta.js
+++ form-helper-1.meta.js
@@ -1,6 +1,6 @@
 // @name         Form Helper
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @updateURL    https://raw.githubusercontent.com/webtools-scripts/browser-helpers/main/form-helper-1.meta.js
-// @downloadURL  https://raw.githubusercontent.com/webtools-scripts/browser-helpers/main/form-helper-1.user.js
+// @version      1.0.1
+// @updateURL    https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-1.meta.js
+// @downloadURL  https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-1.user.js
 // ==/UserScript==
--- form-helper-2.meta.js
+++ form-helper-2.meta.js
@@ -1,6 +1,6 @@
 // @name         Form Auto-Fill
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @updateURL    https://raw.githubusercontent.com/webtools-scripts/browser-helpers/main/form-helper-2.meta.js
-// @downloadURL  https://raw.githubusercontent.com/webtools-scripts/browser-helpers/main/form-helper-2.user.js
+// @version      1.0.1
+// @updateURL    https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-2.meta.js
+// @downloadURL  https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-2.user.js
 // ==/UserScript==
