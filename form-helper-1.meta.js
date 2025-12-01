166a4b1b5cd8a9e739deafc1af4453c546e1c5e4
pchtech <tech@corporates.com>
2025-12-01 14:39:50 -0600

    Update scripts - Mon 12/01/2025 14:39:50.43

--- form-helper-1.meta.js
+++ form-helper-1.meta.js
@@ -1,6 +1,6 @@
 // @name         Form Helper
 // @namespace    http://tampermonkey.net/
-// @version      1.0.3
+// @version      1.0.4
 // @updateURL    https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-1.meta.js
 // @downloadURL  https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-1.user.js
 // ==/UserScript==
