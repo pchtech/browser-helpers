d75bbcfdbb32af1105f5df843b0a9331c1a03680
pchtech <tech@corporates.com>
2025-12-17 16:58:32 -0600

    Update scripts - Wed 12/17/2025 16:58:31.88

--- form-helper-2.meta.js
+++ form-helper-2.meta.js
@@ -1,6 +1,6 @@
 // @name         Form Auto-Fill
 // @namespace    http://tampermonkey.net/
-// @version      1.0.7
+// @version      1.0.8
 // @updateURL    https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-2.meta.js
 // @downloadURL  https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-2.user.js
 // ==/UserScript==
