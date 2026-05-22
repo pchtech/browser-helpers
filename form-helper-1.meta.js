8922d9645bcdc4a8e68b478cf95d94b0fb6ff8db
pchtech <tech@corporates.com>
2026-05-22 17:55:09 -0500

    Update scripts - Fri 05/22/2026 17:55:09.19

--- form-helper-1.meta.js
+++ form-helper-1.meta.js
@@ -1,6 +1,6 @@
 // @name         Form Helper
 // @namespace    http://tampermonkey.net/
-// @version      1.0.10
+// @version      1.0.11
 // @updateURL    https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-1.meta.js
 // @downloadURL  https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-1.user.js
 // ==/UserScript==
--- form-helper-2.meta.js
+++ form-helper-2.meta.js
@@ -1,6 +1,6 @@
 // @name         Form Auto-Fill
 // @namespace    http://tampermonkey.net/
-// @version      1.0.10
+// @version      1.0.11
 // @updateURL    https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-2.meta.js
 // @downloadURL  https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-2.user.js
 // ==/UserScript==
