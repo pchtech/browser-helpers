5e154b9430e3eadbd622f8f21dd41d24f3e63ff2
bellbrokerage <info@bellbrokerage.com>
2026-07-07 12:52:09 -0500

    Update scripts - Tue 07/07/2026 12:52:08.10

--- form-helper-1.meta.js
+++ form-helper-1.meta.js
@@ -1,6 +1,6 @@
 // @name         Form Helper
 // @namespace    http://tampermonkey.net/
-// @version      1.0.14
+// @version      1.0.15
 // @updateURL    https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-1.meta.js
 // @downloadURL  https://raw.githubusercontent.com/pchtech/browser-helpers/main/form-helper-1.user.js
 // ==/UserScript==
